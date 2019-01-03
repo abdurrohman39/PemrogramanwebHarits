354951d6ffdfa018f0c9ca079094e0a620176638
abdurrohman39 <46244961+abdurrohman39@users.noreply.github.com>
2019-01-03 17:52:28 +0700

    PemrogramanwebHarits
    PemrogramanwebHarits

---
+++ aboutme/form/proses.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<title>Keluaran Form</title>
+	</head>
+	<body align="center">
+		<h3>Data Validasi Form</h3>
+		<?php
+			echo "Nama : ".$_POST['nama']."</br>";
+			echo "Email : ".$_POST['email']."</br>";
+			echo "Alamat : ".$_POST['alamat']."</br>";
+			
+		?>
+	</body>
+</html>
